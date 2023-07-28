9f88cd29187a6b6405ff1a117d2790671e0c05c0
aboba88 <ilyapanama88@gmail.com>
2023-07-28 05:13:25 +0300

    add_section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,22 @@
 $(function() {
 
 });
+const box = document.querySelector('box');
+const btn =  document.querySelector('button');
+
+const faqs = document.querySelectorAll(".faq");
+faqs.forEach((faq) => {
+  faq.classList.remove("active");
+});
+
+// Обработчик события клика на кнопку .faq-toggle
+const toggles = document.querySelectorAll(".faq-toggle");
+toggles.forEach((toggle) => {
+  toggle.addEventListener("click", () => {
+    toggle.parentNode.classList.toggle("active");
+  });
+});
+
+
+
+

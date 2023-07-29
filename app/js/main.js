4b10ef3259c4a13d36bbcd7a07a1e7f08eecf77b
TretyakWeb <tretyakbro@gmail.com>
2023-07-29 16:03:26 +0300

    feat: Update app/index.html and app/js/main.js
    - Update DOCTYPE declaration in index.html
    - Reorganize the portfolio images and add a "BOOK AN APPOINTMENT" button
    - Improve the structure and layout of available designs section
    - Refactor and update FAQ section in main.js

--- app/js/main.js
+++ app/js/main.js
@@ -1,21 +1,21 @@
-$(function() {
+$(function () {});
+const box = document.querySelector("box");
+const btn = document.querySelector("button");
 
-});
-const box = document.querySelector('box');
-const btn =  document.querySelector('button');
-
-const faqs = document.querySelectorAll(".faq");
+const faqs = document.querySelectorAll(".faq__item");
 faqs.forEach((faq) => {
   faq.classList.remove("active");
 });
 
+console.log(faqs);
+
 // Обработчик события клика на кнопку .faq-toggle
 const toggles = document.querySelectorAll(".faq-toggle");
+
+console.log(toggles);
+
 toggles.forEach((toggle) => {
   toggle.addEventListener("click", () => {
     toggle.parentNode.classList.toggle("active");
   });
 });
-
-
- 

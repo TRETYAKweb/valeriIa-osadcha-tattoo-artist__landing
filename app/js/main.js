0773ac408560066ecd1a0cb9b9332bc0add64edc
aboba88 <ilyapanama88@gmail.com>
2023-08-14 00:25:03 +0300

    add__scroll

--- app/js/main.js
+++ app/js/main.js
@@ -318,3 +318,19 @@ fileInputsEl.forEach((fileInput, index) => {
     }
   });
 });
+
+//плавная прокрутка для навигации
+document.addEventListener('DOMContentLoaded', function() {
+  const menuLinks = document.querySelectorAll('.menu__list-link');
+
+  menuLinks.forEach(link => {
+    link.addEventListener('click', event => {
+      event.preventDefault();
+      const targetId = link.getAttribute('href');
+      const targetElement = document.querySelector(targetId);
+      if (targetElement) {
+        targetElement.scrollIntoView({ behavior: 'smooth' });
+      }
+    });
+  });
+});
